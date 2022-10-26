b9618b68ecb7813d9ea9787728f73c0e5d2c8ace
BuGalter <wardoc78@gmail.com>
2022-10-26 23:16:59 +0300

    feat: Added some command for work bot logic

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,69 @@
-import { Telegraf, } from 'telegraf';
+import { Telegraf, Markup, } from 'telegraf';
 import bot from './bot';
 
 console.log('Bot start!');
 bot.use(Telegraf.log());
+
+const keyboardUser = Markup.inlineKeyboard([
+  [
+    Markup.button.callback('Информация о боте', 'botInfo'),
+    Markup.button.callback('Подписка на рассылку', 'subscribe')
+  ],
+  [
+    Markup.button.callback('Отписаться от рассылки', 'unSubscribe'),
+    Markup.button.callback('Выход', 'quit')
+  ]
+]);
+
+const keyboardAdmin = Markup.inlineKeyboard([
+  [
+    Markup.button.callback('Информация о боте', 'botInfo'),
+    Markup.button.callback('Создать сообщение', 'createMessage')
+  ],
+  [
+    Markup.button.callback('Начать рассылку', 'startMailing'),
+    Markup.button.callback('Выход', 'quit')
+  ]
+]);
+
+bot.start(async (ctx) => {
+  await ctx.reply(`Это чат-бот группы:\n 
+набери /user если ты пользователь группы.\n
+набери /admin если ты администратор группы.`);
+});
+
+bot.command('user', async (ctx) => {
+  await ctx.reply('Меню пользователя', keyboardUser);
+});
+
+bot.command('admin', async (ctx) => {
+  await ctx.reply('Меню администратора', keyboardAdmin);
+});
+
+bot.action('botInfo', async (ctx) => {
+  await ctx.reply('Bot info');
+});
+
+bot.action('quit', async (ctx) => {
+  await ctx.reply('Спасибо, что использовали бота!');
+});
+
+bot.action('subscribe', async (ctx) => {
+  await ctx.reply('Спасибо, что подписались на рассылку!');
+});
+
+bot.action('unSubscribe', async (ctx) => {
+  await ctx.reply('Спасибо, что отписались от рассылки!');
+});
+
+bot.action('createMessage', async (ctx) => {
+  await ctx.reply('Введите текст сообщения для рассылки:');
+});
+
+bot.action('startMailing', async (ctx) => {
+  await ctx.reply('Отправляем!');
+});
+
 bot.launch();
 
 // Enable graceful stop
